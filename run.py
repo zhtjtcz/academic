a4dca248209b1c479e8f20e35e563294c5b99205
zhtjtcz <18377221@buaa.edu.cn>
2021-10-02 15:48:08 +0800

    upd: OS check

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ os.system("pip install -r requirements.txt")
 os.system("python manage.py makemigrations")
 os.system("python manage.py migrate")
 
-if platform.system() == "Windows":
+if platform.system() != "Linux":
 	os.system("python manage.py runserver")
 else:
 	os.system("nohup python manage.py runserver 0.0.0.0:8000 & \n")
