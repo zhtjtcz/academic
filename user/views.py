d597841294e835f65917f1e595633ce2ed342990
zhtjtcz <18377221@buaa.edu.cn>
2021-10-03 20:09:30 +0800

    fix: captcha error

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from captcha.models import CaptchaStore
+# from captcha.models import CaptchaStore
 from academic.values import *
 # Create your views here.
 
