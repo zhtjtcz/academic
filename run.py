31cf32981917f1ff8cae1785df112fbfe3a39130
zhtjtcz <18377221@buaa.edu.cn>
2021-10-03 20:28:04 +0800

    fix: deploy TLE

--- run.py
+++ run.py
@@ -10,5 +10,5 @@ os.system("python manage.py migrate")
 if platform.system() != "Linux":
 	os.system("python manage.py runserver")
 else:
-	os.system("nohup python manage.py runserver 0.0.0.0:8000 &")
+	os.system("nohup python manage.py runserver 0.0.0.0:8000 & \n")
 	print("The backend is running!")
