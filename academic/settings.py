66b94d7afdf7003b6b4e43f405979a30408999f2
zhtjtcz <18377221@buaa.edu.cn>
2021-10-03 19:57:08 +0800

    feat: kill.py

--- academic/settings.py
+++ academic/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
 	'user',
 	'corsheaders',
-	'captcha',
+	# 'captcha',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
