58d202f3fd4ea79c24d31488e7a11f3afb613b0b
zhtjtcz <18377221@buaa.edu.cn>
2021-11-30 21:11:46 +0800

    add: search by page

--- search/search.py
+++ search/search.py
@@ -5,16 +5,17 @@ from academic.values import *
 需要支持的功能： 按标题等各个字段检索，多字段检索
 搜索结果高亮 : 前端 
 '''
-def nomalSearch(title = "", author = "", abstract = ""):
+
+def nomalSearch(title = "", author = "", abstract = "",
+				page = 1):
 	mapping = {
 		"query": {
-			"match": {
-				
-			}
+			"match": {}
 		},
 		"from": 0,
-		"size": 100,
+		"size": 10000
 	}
+
 	if len(title) > 0:
 		mapping["query"]["match"]["title"] = {
 			"query": title,
@@ -34,7 +35,8 @@ def nomalSearch(title = "", author = "", abstract = ""):
 	papers = origin["hits"]["hits"]
 	papers = [x["_source"] for x in papers]
 	result = {
-		"paper": papers,
-		"total": len(papers)
+		"paper": papers[PAGE*(page-1) : PAGE*page],			# devide by page
+		"total": len(papers),
+		"pages": (len(papers) + PAGE - 1) // PAGE
 	}
 	return result
