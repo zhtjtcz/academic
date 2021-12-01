3fdcae1ad126ea5c1b4576de26043261bca8518e
zhtjtcz <18377221@buaa.edu.cn>
2021-12-01 23:08:19 +0800

    fix: register

--- user/models.py
+++ user/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(null=True, blank=True)
     gender = models.IntegerField(null=True, blank=True)
     phone = models.CharField(max_length=30, null=True, blank=True)
     area = models.CharField(max_length=100, null=True, blank=True)
