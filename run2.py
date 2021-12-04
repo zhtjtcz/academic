29e7a614b6c091c545a2f02f703a8c2e56400c77
zhtjtcz <18377221@buaa.edu.cn>
2021-12-04 21:22:43 +0800

    add: run2.py

---
+++ run2.py
@@ -0,0 +1,14 @@
+import os
+import platform
+
+# pipreqs . --encoding=utf8 --force
+# os.system("pip install -r requirements.txt")
+
+os.system("python3 manage.py makemigrations")
+os.system("python3 manage.py migrate")
+
+if platform.system() != "Linux":
+	os.system("python3 manage.py runserver")
+else:
+	os.system("python3 manage.py runserver 0.0.0.0:8000 > log.txt & \n")
+	print("The backend is running!")
