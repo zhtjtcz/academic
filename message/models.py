904ad4f5d0c600b863d519873148ca1931b51b5f
thweetkomputer <2320092610@qq.com>
2021-12-04 19:45:47 +0800

    message添加文章名

--- message/models.py
+++ message/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Message(models.Model):
 	id = models.AutoField(primary_key=True)
+	title = models.CharField(max_length=100, blank=True, null=True)
 	type = models.IntegerField()
 	uid = models.IntegerField()
 	pid = models.IntegerField(null=True, blank=True, default = 0)
