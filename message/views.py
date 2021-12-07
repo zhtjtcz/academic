021412c047407a7c9b871e700b5bbff75a10be5f
zhtjtcz <18377221@buaa.edu.cn>
2021-12-07 12:35:09 +0800

    fix: isdeal deal

--- message/views.py
+++ message/views.py
@@ -120,9 +120,9 @@ def deal_claim(request):
     id = int(data_json['id'])
     result = int(data_json['result'])
     message = Message.objects.get(id=id)
-    if message.isdeal == True:
+    if message.isdeal != 0:
         return JsonResponse({'result': ACCEPT, 'message': r'已完成处理！'})
-    message.isdeal = True
+    message.isdeal = result
     message.isread = True
     message.save()
     if result == 1:
