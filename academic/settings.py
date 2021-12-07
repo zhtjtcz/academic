0b3de5d5806fe9b5589f3a382ec24471a4c0bdc9
zhtjtcz <18377221@buaa.edu.cn>
2021-12-07 22:56:26 +0800

    fix: root

--- academic/settings.py
+++ academic/settings.py
@@ -17,7 +17,7 @@ import platform
 import redis
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
