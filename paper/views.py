6ece95c82daae5af0664a7ccc37bded0a41cec41
thweetkomputer <2320092610@qq.com>
2021-12-11 14:34:39 +0800

    get_favor_list() 获得paper信息

--- paper/views.py
+++ paper/views.py
@@ -205,8 +205,8 @@ def get_favor_list(request):
 	begin = int(data_json['begin'])
 	end = int(data_json['end'])
 	# res = [x for x in Favor.objects.filter(uid=id)][begin:end]
-	res = Favor.objects.filter(uid=id).values()
-	return JsonResponse({'result': ACCEPT, 'message': r'获取成功！', 'list': list(res)[begin:end]})
+	res = [Paper.objects.filter(id=i.pid).values() for i in Favor.objects.filter(uid=id)[begin:end]]
+	return JsonResponse({'result': ACCEPT, 'message': r'获取成功！', 'list': res})
 
 @csrf_exempt
 def get_comments(request):
