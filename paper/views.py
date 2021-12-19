a4a200d05a8a5743279f843510792ded15f03ac6
zhtjtcz <18377221@buaa.edu.cn>
2021-12-19 17:49:51 +0800

    del: hot paper

--- paper/views.py
+++ paper/views.py
@@ -183,7 +183,6 @@ def get_paper_info(request):
 
 @csrf_exempt
 def get_hot_paper(request):
-	Redis.zremrangebyscore(name = 'paper', min = 0, max = 1000000)
 	result = Redis.zrevrange(name="paper", start=1, end=10, withscores=True, score_cast_func=float)
 	result = [x for x in result]
 	clear = []
